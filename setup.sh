93b72a0407eb059f8d19fc34b4d21448a6d91ff4
Francesco Bianco <info.francescobianco@gmail.com>
2021-09-28 21:04:58 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,37 @@
 ## Remove unused
 gnome-extensions uninstall system-monitor@paradoxxx.zero.gmail.com
 gnome-extensions uninstall gTile@vibou
+gnome-extensions uninstall sound-output-device-chooser@kgshank.net
+
+StatusTitleBarAndDropdown@devpower.org
+app-menu-window-title@eternal-sorrow.github.com
+StatusTitleBar@devpower.org
+window-title-in-topbar@fthx
+gTile@vibou
+gsconnect@andyholmes.github.io
+freon@Veske
+desktop-icons@csoriano
+ubuntu-appindicators@ubuntu.com
+ubuntu-dock@ubuntu.com
+dash-to-panel@jderose9.github.com
+hidetopbar@mathieu.bidon.ca
+apps-menu@gnome-shell-extensions.gcampax.github.com
+auto-move-windows@gnome-shell-extensions.gcampax.github.com
+drive-menu@gnome-shell-extensions.gcampax.github.com
+horizontal-workspaces@gnome-shell-extensions.gcampax.github.com
+launch-new-instance@gnome-shell-extensions.gcampax.github.com
+native-window-placement@gnome-shell-extensions.gcampax.github.com
+places-menu@gnome-shell-extensions.gcampax.github.com
+screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com
+user-theme@gnome-shell-extensions.gcampax.github.com
+window-list@gnome-shell-extensions.gcampax.github.com
+windowsNavigator@gnome-shell-extensions.gcampax.github.com
+workspace-indicator@gnome-shell-extensions.gcampax.github.com
+transparent_panel@fawtytoo
+gnome-plat-workspace@stonegate.me
+transparent-top-bar@zhanghai.me
+straighttopbar@peclu.net.net
 
 
-
-
+## 
+#dash-to-dock@micxgx.gmail.com
